5e71a003bfb8fad2dd2cc78b3817d324d7538afe
Reza <r.atae@yahoo.com>
2021-03-07 04:08:55 +0000

    Created basic patrolling enemy prototype
    Added basic enemy patrolling between two waypoints.

---
+++ ClockDown2.0/Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Transform[] positions;
+
+    Transform nextPos;
+
+    public int speed;
+
+    private int nextPosIndex;
+    private float dist;
+
+
+    void Start(){
+
+        nextPos = positions[0];
+
+    }
+
+    void Update(){
+
+        MoveEnemy();
+
+    }
+
+    void MoveEnemy(){
+        if (transform.position == nextPos.position){
+            nextPosIndex++;
+            if (nextPosIndex >= positions.Length){
+                nextPosIndex = 0;
+            }
+            nextPos = positions[nextPosIndex];
+        }
+        else{
+            transform.position = Vector3.MoveTowards(transform.position, nextPos.position, speed*Time.deltaTime);
+        }
+
+    }
+    
+}
