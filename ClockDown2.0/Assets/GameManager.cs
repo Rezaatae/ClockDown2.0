31e4ca075fc4870cfe99df2806a972f356625b05
Reza <r.atae@yahoo.com>
2021-03-08 00:16:11 +0000

    restart to level 1 when you die
    now when you die in a different level other than level 1, you will respawn to level 1.

--- ClockDown2.0/Assets/GameManager.cs
+++ ClockDown2.0/Assets/GameManager.cs
@@ -9,10 +9,13 @@ public class GameManager : MonoBehaviour
     bool gameHasEnded = false;
     public void GameOver(){
 
-        if(gameHasEnded == false){
+        if(gameHasEnded == false && SceneManager.GetActiveScene().name == "SampleScene"){
             gameHasEnded = true;
             Debug.Log("Game Over");
             Restart();
+        }
+        else{
+            SceneManager.LoadScene("SampleScene");
         }
         
 
