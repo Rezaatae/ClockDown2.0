eeaf7202c2eb865ea8e07351a65f8bb5ea295c04
Kauna Mohammed <kaunamohammed@gmail.com>
2021-03-19 17:50:25 +0100

    refactors

--- Assets/Scripts/LevelComplete.cs
+++ Assets/Scripts/LevelComplete.cs
@@ -6,7 +6,6 @@ public class LevelComplete : MonoBehaviour
 {
     public void LoadNextLevel()
     {
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         PhotonNetwork.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
