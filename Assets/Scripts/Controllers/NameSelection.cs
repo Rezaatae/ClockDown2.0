0617cbfbc13f0c5fbfe2e86b5882e6565881c4f0
Kauna Mohammed <kaunamohammed@gmail.com>
2021-03-21 08:34:03 +0100

    dedicated scene for adding  player name

---
+++ Assets/Scripts/Controllers/NameSelection.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using TMPro;
+
+public class NameSelection : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject inputPanel;
+
+    [SerializeField]
+    private TMP_InputField playerNickNameTextField;
+
+    [SerializeField]
+    private Button continueButton;
+
+    public void OnClickContinue()
+    {
+        PhotonNetwork.NickName = playerNickNameTextField.text;
+        SceneManager.LoadScene(Constants.Scenes.MainMenu);
+    }
+
+}
