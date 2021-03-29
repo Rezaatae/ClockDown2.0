76fe174e52c923e5129583ec6e6c7c0a7ac2d323
Kauna Mohammed <kaunamohammed@gmail.com>
2021-03-29 11:23:55 +0100

    added leader table

---
+++ Assets/Scripts/UI/LeaderTableList.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using Photon.Pun;
+using System.Collections.Generic;
+
+public class LeaderTableList : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Transform content;
+
+    [SerializeField]
+    private PlayerPoints playerPointsPrefab;
+
+    private void Start()
+    {
+
+        List<Photon.Realtime.Player> playerList = new List<Photon.Realtime.Player>(PhotonNetwork.PlayerList);
+        playerList.Sort(ByPlayerPoints);
+        
+        foreach (var player in playerList)
+        {
+            PlayerPoints playerPoints = Instantiate(playerPointsPrefab, content);
+
+            if (playerPoints != null)
+            {
+                if (playerList[0].Equals(player))
+                    playerPoints.color = Color.yellow;
+                else
+                    playerPoints.color = Color.black;
+            
+                playerPoints.SetPointsFor(player);
+            }
+            
+        }
+    }
+
+    private int ByPlayerPoints(Photon.Realtime.Player p1, Photon.Realtime.Player p2)
+    {
+        return ((int) p1.CustomProperties[Constants.Scenes.Game.Objects.PlayerCurrentScore]).CompareTo((int) p2.CustomProperties[Constants.Scenes.Game.Objects.PlayerCurrentScore]);
+    }
+
+}
--- Assets/Scripts/UI/Player/PlayerPoints.cs
+++ Assets/Scripts/UI/Player/PlayerPoints.cs
@@ -9,12 +9,15 @@ public class PlayerPoints : MonoBehaviourPunCallbacks
     [SerializeField]
     private TextMeshProUGUI text;
 
+    public Color color;
+
     public Photon.Realtime.Player Player { get; private set; }
     
     public void SetPointsFor(Photon.Realtime.Player player)
     {
         Player = player;
         SetPlayerPointsText(player);
+        text.color = color;
     }
 
     public override void OnPlayerPropertiesUpdate(Photon.Realtime.Player targetPlayer, Hashtable changedProps)
