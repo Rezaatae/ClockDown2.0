2bad52860dff49c06771d7929f8a705e618c2c0c
Kauna Mohammed <kaunamohammed@gmail.com>
2021-03-30 09:50:03 +0100

    bug fixes

--- Assets/Scripts/UI/LeaderTableList.cs
+++ Assets/Scripts/UI/LeaderTableList.cs
@@ -25,6 +25,8 @@ public class LeaderTableList : MonoBehaviour
             {
                 if (playerList[0].Equals(player))
                     playerPoints.color = Color.yellow;
+                else
+                    playerPoints.color = Color.black;
             
                 playerPoints.SetPointsFor(player);
             }
